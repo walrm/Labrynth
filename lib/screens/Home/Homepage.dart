bbcbecd5a3e5092174249133cfff6656924256aa
Kunal Thakker <kthakker3@gmail.com>
2020-06-15 17:10:48 -0400

    added savestate stuff to homscreen

--- lib/screens/Home/Homepage.dart
+++ lib/screens/Home/Homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:simple_gesture_detector/simple_gesture_detector.dart';
+import 'package:Labrynth_test/screens/Game/saveState.dart';
 
 class Homepage extends StatefulWidget{
   @override
@@ -11,6 +12,7 @@ class Homepage extends StatefulWidget{
 class Homestate extends State<Homepage> {
   int currentWorld=0;
   int totalWorlds=10;
+  bool isInit=true;
 
   _horizontalSwipe(SwipeDirection direction){
     if(direction==SwipeDirection.left && currentWorld!=0){
@@ -35,61 +37,88 @@ class Homestate extends State<Homepage> {
     super.dispose();
   }
 
+  Future<SaveState> initSaveState() async{
+    SaveState save=new SaveState();
+    await save.init();
+    return save;
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView.builder(
-      itemBuilder: (context, index) {
-        int currentWorld = index+1;
-        Icon icon;
-        ImageProvider img;
-        bool goToGame;
-        if (currentWorld == 1){
-          icon = Icon(Icons.play_circle_outline, color:Colors.green);
-          img = AssetImage("assets/nature.jpg");
-          goToGame = true;
-        }
-        else if (currentWorld == 2){
-          icon = Icon(Icons.lock_outline, color:Colors.grey);
-          img = AssetImage("assets/ocean.jpg");
-          goToGame = false;
+    return FutureBuilder<SaveState>(
+      future: initSaveState(),
+      builder: (BuildContext context, AsyncSnapshot<SaveState> snapshot){
+        if(snapshot.hasData){
+            return Scaffold(
+              body: PageView.builder(
+              itemBuilder: (context, index) {
+                int currentWorld = index+1;
+                Icon icon;
+                ImageProvider img;
+                bool goToGame;
+                if (currentWorld == 1){
+                  icon = Icon(Icons.play_circle_outline, color:Colors.green);
+                  img = AssetImage("assets/nature.jpg");
+                  goToGame = true;
+                }
+                else if (currentWorld == 2){
+                  icon = Icon(Icons.lock_outline, color:Colors.grey);
+                  img = AssetImage("assets/ocean.jpg");
+                  goToGame = false;
 
+                }
+                else{
+                  icon = Icon(Icons.lock_outline, color:Colors.grey);
+                  img = AssetImage("assets/ocean.jpg");
+                  goToGame = false;
+                }
+                return Stack(
+                  children: <Widget> [
+                    Container(
+                      child: Center(
+                        child: IconButton(
+                          iconSize: 120,
+                          icon: icon,
+                          onPressed: (){
+                            if (goToGame){
+                              bool test=false;
+                              Navigator.pushNamed(
+                                context,
+                                '/game',
+                                arguments: <bool>{
+                                  test,
+                                }
+                                );
+                            }
+                            
+                          },
+                        )
+                      ),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: img,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Text('$currentWorld/$totalWorlds', style: TextStyle(fontSize: 60.0, color: Colors.white))
+                    ),
+                  ]
+                );
+              },
+              itemCount: 10, // Can be null
+              )
+            );
         }
         else{
-          icon = Icon(Icons.lock_outline, color:Colors.grey);
-          img = AssetImage("assets/ocean.jpg");
-          goToGame = false;
+          return SizedBox(
+            child: CircularProgressIndicator(),
+            width: 60,
+            height: 60,
+          );
         }
-        return Stack(
-          children: <Widget> [
-            Container(
-              child: Center(
-                child: IconButton(
-                  iconSize: 120,
-                  icon: icon,
-                  onPressed: (){
-                    if (goToGame){
-                      Navigator.pushNamed(context, '/game');
-                    }
-                    
-                  },
-                )
-              ),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: img,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              child: Text('$currentWorld/$totalWorlds', style: TextStyle(fontSize: 60.0, color: Colors.white))
-            ),
-          ]
-        );
-      },
-      itemCount: 10, // Can be null
-      )
+      }
     );
+
   }
 }
