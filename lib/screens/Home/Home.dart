beea44be4d1d69283f6704c1978db95dee379597
Kunal Thakker <kthakker3@gmail.com>
2020-06-21 23:42:56 -0400

    homepage goes to worlds page

--- lib/screens/Home/Home.dart
+++ lib/screens/Home/Home.dart
@@ -1,7 +1,9 @@
-import 'package:Labrynth_test/world.dart';
 import 'package:flutter/material.dart';
 import 'package:Labrynth_test/screens/Game/saveState.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
+import 'package:Labrynth_test/screens/Home/Homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class Home extends StatefulWidget{
@@ -13,17 +15,17 @@ class Home extends StatefulWidget{
 
 class HomeScreen extends State<Home>{
 
+
   Future<SaveState> initSaveState() async{
     SaveState save=new SaveState();
     await save.init();
     return save;
   }
 
-
-
-
   @override
   Widget build(BuildContext context){
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return FutureBuilder<SaveState>(
       future: initSaveState(),
       builder: (BuildContext context, AsyncSnapshot<SaveState> snapshot) {
@@ -43,9 +45,198 @@ class HomeScreen extends State<Home>{
                       image: AssetImage('assets/scuffedBackground.png'),
                     ),
                   ),
-                ),
-                Container(//Home Page?
-                  child: Align
+                  child: Container(
+                    width: screenWidth,
+                    height: screenHeight,
+                    /*child: Stack(//Trying out buttons at the bottom
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment(0,.33),
+                          child:Stack(
+                            children: <Widget>[
+                              Opacity(
+                                opacity:.95,
+                                child: Icon(
+                                    Icons.play_circle_filled,
+                                    size:100,
+                                    color: Colors.grey,
+                                ),
+                              ),
+                              Opacity(
+                                opacity:1,
+                                child: IconButton(
+                                  padding: EdgeInsets.all(0),
+                                  icon: Icon(
+                                    Icons.play_circle_outline,
+                                    color: Colors.black,
+                                  ),
+                                  iconSize: 100,
+                                  onPressed: (){
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context)=>Homepage()
+                                      )
+                                    );
+                                  },
+                                ),
+                              ),
+                            ]), 
+                        ),
+                        Align(
+                          alignment: Alignment(-.7,.6),
+                          child:Stack(
+                            children: <Widget>[
+                              Opacity(
+                                opacity:1,
+                                child: IconButton(
+                                  padding: EdgeInsets.all(0),
+                                  icon: Icon(
+                                    Icons.settings,
+                                    color: Colors.black,
+                                  ),
+                                  iconSize: 80,
+                                  onPressed: (){
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context)=>Homepage()
+                                      )
+                                    );
+                                  },
+                                ),
+                              ),
+                            ]), 
+                        ),
+                        Align(
+                          alignment: Alignment(.7,.6),
+                          child:Stack(
+                            children: <Widget>[
+                              Opacity(
+                                opacity:1,
+                                child: IconButton(
+                                  padding: EdgeInsets.all(0),
+                                  icon: Icon(
+                                    Icons.shopping_cart,
+                                    color: Colors.black,
+                                  ),
+                                  iconSize: 80,
+                                  onPressed: (){
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context)=>Homepage()
+                                      )
+                                    );
+                                  },
+                                ),
+                              ),
+                            ]), 
+                        ),
+                    ]),*/
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment(0,-.3),
+                          child:InkWell(
+                            onTap:(){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context)=>Homepage()
+                                )
+                              );
+                            },
+                            child: Container(
+                              height:80,
+                              width:200,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [Colors.red,Colors.deepOrange,Colors.yellow],
+                                  stops:[0.0,.3,1],
+                                  begin: FractionalOffset.topCenter,
+                                  end: FractionalOffset.bottomCenter,
+                                ),
+                                border: Border.all(width:6,color:Colors.black),
+                              ),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'Play',
+                                  style:  GoogleFonts.roboto(
+                                    fontSize: 40,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment(0,.1),
+                          child: Container(
+                            height:80,
+                            width:200,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Colors.red,Colors.deepOrange,Colors.yellow],
+                                stops:[0.0,.3,1],
+                                begin: FractionalOffset.topCenter,
+                                end: FractionalOffset.bottomCenter,
+                              ),
+                              border: Border.all(width:6,color:Colors.black),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Settings',
+                                style:  GoogleFonts.roboto(
+                                  fontSize: 40,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment(0,.5),
+                          child:InkWell(
+                            onTap:(){
+
+
+
+
+                            },
+                            child: Container(
+                              height:80,
+                              width:200,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [Colors.red,Colors.deepOrange,Colors.yellow],
+                                  stops:[0.0,.3,1],
+                                  begin: FractionalOffset.topCenter,
+                                  end: FractionalOffset.bottomCenter,
+                                ),
+                                border: Border.all(width:6,color:Colors.black),
+                              ),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'Shop',
+                                  style:  GoogleFonts.roboto(
+                                    fontSize: 40,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+
+
+
+
+
+                    ),
+                  ),
                 ),
             ]),
           );
